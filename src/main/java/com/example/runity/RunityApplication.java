6d5640b3442ab34b4652d4b4594208deef20b97b
choihyojeong <chj030405@yu.ac.kr>
2025-05-20 23:37:29 +0900

    feat: 스프링부트 + Gradle 기반 프로젝트 구조 초기 설정

---
+++ src/main/java/com/example/runity/RunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.runity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/runity/RunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
