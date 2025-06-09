cd01d5f2fdf3c54e4cbdcaff800e937f16b8f438
choihyojeong <chj030405@yu.ac.kr>
2025-06-09 12:30:29 +0900

    feat: 실시간 러닝을 위한 정보를 불러오는 API 추가

---
+++ src/main/java/com/example/runity/DTO/RunningSettingsResponse.java
@@ -0,0 +1,13 @@
+package com.example.runity.DTO;
+
+//import com.example.runity.domain.Route;
+import com.example.runity.domain.Routine;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RunningSettingsResponse {
+    //private Route route;               // 추천 경로 정보
+    private Routine routine;           // 루틴 정보
+    private Double recommendedPace;    // 추천 페이스 (/km)
+}
