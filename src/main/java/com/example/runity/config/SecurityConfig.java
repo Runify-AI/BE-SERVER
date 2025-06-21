d29c11d8b33e0e0efd31f9a758f637716f493d82
choihyojeong <chj030405@yu.ac.kr>
2025-06-21 13:31:59 +0900

    feat: 사용자 접속 허용 범위 관련 기능 수정

--- src/main/java/com/example/runity/config/SecurityConfig.java
+++ src/main/java/com/example/runity/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://3.37.214.150:8080", "http://localhost:5173", "http://127.0.0.1:5173", "https://387d-165-229-250-144.ngrok-free.app/")); // 허용할 도메인 설정
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://3.37.214.150:8080", "http://localhost:5173", "http://127.0.0.1:5173","http://localhost:5174", "https://f664-165-229-112-243.ngrok-free.app/")); // 허용할 도메인 설정
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
@@ -95,6 +95,5 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
 
---
+++ src/main/java/com/example/runity/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.runity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("*") // 모든 Origin 허용 (운영에서는 도메인 지정 권장)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
