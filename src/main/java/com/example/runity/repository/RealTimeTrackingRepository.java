7711b4dc6f6bc935209aa2e0dfe06088cf545105
choihyojeong <chj030405@yu.ac.kr>
2025-10-11 12:07:36 +0900

    feat: 사용하지 않는 Repository 클래스 삭제

--- src/main/java/com/example/runity/repository/RealTimeTrackingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.runity.repository;
-
-import com.example.runity.domain.RealTimeTracking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RealTimeTrackingRepository extends JpaRepository<RealTimeTracking, Long> {
-}
