9e664911c7bdf9deddd84bb8839493e8ec334265
choihyojeong <chj030405@yu.ac.kr>
2025-11-04 10:24:18 +0900

    feat: ReturnCodeDTO를 제네릭으로 수정

--- src/main/java/com/example/runity/DTO/ReturnCodeDTO.java
+++ src/main/java/com/example/runity/DTO/ReturnCodeDTO.java
@@ -7,16 +7,22 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
-public class ReturnCodeDTO {
+public class ReturnCodeDTO<T> {
     private int status;
     private String message;
     private Object data;
+    private T data;
+
+    public ReturnCodeDTO(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
 
     public ReturnCodeDTO(int status, String message) {
         this.status = status;
         this.message = message;
+        this.data = null; // data가 없을 경우
     }
-
 }
